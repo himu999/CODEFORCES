7a5580016e60d1958f698e6aef2eb004239cabef
himel999 <mahabubulalamofficial@gmail.com>
2021-03-12 13:13:08 +0600

    59A

---
+++ 59A_Word.py
@@ -0,0 +1,20 @@
+string = input()
+upper = 0
+lower = 0
+
+i = 0
+
+while i < len(string):
+
+    if string[i].isupper():
+        upper += 1
+    elif string[i].islower():
+        lower += 1
+    i += 1
+
+if upper > lower:
+    print(string.upper())
+else:
+    print(string.lower())
+
+
